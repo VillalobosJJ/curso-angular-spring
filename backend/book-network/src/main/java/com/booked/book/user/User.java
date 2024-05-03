6a93fd71add21e5a91aab0bb89ee85eac842c351
Javier Villalobos <arnulfo.juarez@coppel.com>
2024-05-02 17:28:53 -0700

    Se crean configuraciones yaml

---
+++ backend/book-network/src/main/java/com/booked/book/user/User.java
@@ -0,0 +1,53 @@
+package com.booked.book.user;
+
+import jakarta.persistence.Entity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.security.Principal;
+import java.util.Collection;
+import java.util.List;
+
+public class User implements UserDetails, Principal {
+
+
+    @Override
+    public String getName() {
+        return "";
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return "";
+    }
+
+    @Override
+    public String getUsername() {
+        return "";
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+}
